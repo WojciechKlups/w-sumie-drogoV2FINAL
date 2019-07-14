857f06b96e09a0712de3fa200e624e015769774e
Wojciech Klupś <wojciech.klups@gmail.com>
2019-07-14 15:48:03 +0200

    login and event listener

---
+++ src/main/java/pl/sda/wsumiedrogo/config/EventListener.java
@@ -0,0 +1,32 @@
+package pl.sda.wsumiedrogo.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.annotation.Configuration;
+import pl.sda.wsumiedrogo.model.User;
+import pl.sda.wsumiedrogo.repositories.UserRepository;
+@Configuration
+public class EventListener {
+
+    private UserRepository userRepository;
+
+    @Autowired
+    public EventListener(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @org.springframework.context.event.EventListener(ApplicationReadyEvent.class)
+    public void init(){
+        User user = new User();
+        user.setFirstName("Jan");
+        user.setLastName("Kowalski");
+        user.setUsername("kowalski");
+        user.setEmail("kowalski@gmail.com");
+        user.setPassword("1234");
+        user.setAddress("ul.Januszowa 5");
+        user.setPostalCode("60-123");
+        user.setCity("Poznan");
+        user.setPhoneNumber(502676950L);
+        userRepository.save(user);
+    }
+}
