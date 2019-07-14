1b20d4d1c1bbd559e9984d33f7f2945197614e70
Wojciech Klupś <wojciech.klups@gmail.com>
2019-07-14 14:28:23 +0200

    Initial commit

---
+++ src/main/java/pl/sda/wsumiedrogo/WSumieDrogoApplication.java
@@ -0,0 +1,13 @@
+package pl.sda.wsumiedrogo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WSumieDrogoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WSumieDrogoApplication.class, args);
+    }
+
+}
