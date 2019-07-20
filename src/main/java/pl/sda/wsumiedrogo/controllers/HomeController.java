d7bdc7026dad1151f921a7842c4fd96f4df26464
Mateusz Haberla <Mateusz.Haberla22@gmail.com>
2019-07-20 12:21:49 +0200

    Homecontroller

--- src/main/java/pl/sda/wsumiedrogo/controllers/HomeController.java
+++ src/main/java/pl/sda/wsumiedrogo/controllers/HomeController.java
@@ -1,16 +1,30 @@
 package pl.sda.wsumiedrogo.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import pl.sda.wsumiedrogo.model.User;
 import pl.sda.wsumiedrogo.repositories.UserRepository;
+import pl.sda.wsumiedrogo.service.UserService;
 
 @Controller
 public class HomeController {
 
+
     private UserRepository userRepository;
+    private UserService userService;
+
+    @Autowired
+    public HomeController(UserRepository userRepository, UserService userService) {
+        this.userRepository = userRepository;
+        this.userService = userService;
+    }
+
 
     @GetMapping("/")
     public String home() {
@@ -24,13 +38,14 @@ public class HomeController {
     }
 
     @PostMapping("/register")
-    public String postRegister() {
-
-        return "register";
+    @ResponseStatus(HttpStatus.CREATED)
+    public User postRegister(@RequestParam User user) {
+        return userService.createNewUser(user);
     }
 
-    @GetMapping("/successpage")
+    @PostMapping("/successpage")
     public String successpage() {
+
         return "successpage";
     }
 
