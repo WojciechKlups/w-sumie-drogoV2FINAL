dff49297955cccae34f1f42ec72c3b5648f2ee74
Wojciech Klupś <wojciech.klups@gmail.com>
2019-07-20 11:50:52 +0200

    MODIFIED:

--- src/main/java/pl/sda/wsumiedrogo/controllers/HomeController.java
+++ src/main/java/pl/sda/wsumiedrogo/controllers/HomeController.java
@@ -5,7 +5,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import pl.sda.wsumiedrogo.model.dto.UserDto;
-import pl.sda.wsumiedrogo.service.UserService;
 
 @Controller
 public class HomeController {
