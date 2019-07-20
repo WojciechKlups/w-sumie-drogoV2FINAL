131c8dbaf862be254345b429c228c7c4b69c0ba8
Mateusz Haberla <Mateusz.Haberla22@gmail.com>
2019-07-20 14:55:08 +0200

    REGISTRATION AND LOGIN IS WORKING !

--- src/main/java/pl/sda/wsumiedrogo/controllers/HomeController.java
+++ src/main/java/pl/sda/wsumiedrogo/controllers/HomeController.java
@@ -4,10 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.*;
 import pl.sda.wsumiedrogo.model.User;
 import pl.sda.wsumiedrogo.repositories.UserRepository;
 import pl.sda.wsumiedrogo.service.UserService;
@@ -39,9 +36,16 @@ public class HomeController {
         return "account";
     }
 
+    @GetMapping("/register")
+    @ResponseStatus(HttpStatus.CREATED)
+    public String getRegister() {
+        return "register";
+    }
+
+
     @PostMapping("/register")
     @ResponseStatus(HttpStatus.CREATED)
-    public String postRegister(@RequestParam User user,Model model) {
+    public String postRegister(@ModelAttribute User user, Model model) {
         userService.createNewUser(user);
         model.addAttribute("user",user);
         return "successpage";
