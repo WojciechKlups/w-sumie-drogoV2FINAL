b4e9bc35f308c6d9a2ac41d373d623743390c8ad
Mateusz Haberla <Mateusz.Haberla22@gmail.com>
2019-07-21 12:20:34 +0200

    Added spring security and hashing passwords

---
+++ src/main/java/pl/sda/wsumiedrogo/security/WebSecurityConfig.java
@@ -0,0 +1,34 @@
+package pl.sda.wsumiedrogo.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(10);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/").permitAll()
+                .antMatchers("/login").permitAll()
+                .antMatchers("/cart").permitAll()
+                .antMatchers("/store").permitAll()
+                .antMatchers("/register").permitAll()
+                .antMatchers("/checkout").permitAll()
+                .antMatchers("/successpage").permitAll()
+                .antMatchers("account").hasRole("USER")
+                .and()
+                .csrf().disable();
+    }
+}
