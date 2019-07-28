f28b8e8a3bd4249d729090b13c649ceade9ed34f
Mateusz Haberla <Mateusz.Haberla22@gmail.com>
2019-07-28 15:31:55 +0200

    controller refactoring

--- src/main/java/pl/sda/wsumiedrogo/controllers/MainController.java
+++ src/main/java/pl/sda/wsumiedrogo/controllers/MainController.java
@@ -1,52 +1,31 @@
 package pl.sda.wsumiedrogo.controllers;
 
-import org.apache.commons.mail.EmailException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
-import pl.sda.wsumiedrogo.model.Cart;
-import pl.sda.wsumiedrogo.model.User;
-import pl.sda.wsumiedrogo.security.UserDetailsServiceImpl;
-import pl.sda.wsumiedrogo.security.WebSecurityConfig;
-import pl.sda.wsumiedrogo.service.*;
-import org.springframework.web.bind.annotation.RequestParam;
-import pl.sda.wsumiedrogo.model.dto.UserDto;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.GetMapping;
+import pl.sda.wsumiedrogo.service.CookieService;
+import pl.sda.wsumiedrogo.service.LoginService;
+import pl.sda.wsumiedrogo.service.UserService;
 
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.security.Principal;
 
 @Controller
 public class MainController {
 
-    private WebSecurityConfig webSecurityConfig;
+
     private UserService userService;
     private CookieService cookieService;
-    private UserDetailsServiceImpl userDetailsService;
     private LoginService loginService;
-    private CheckoutService checkoutService;
-    private RegistrationService registrationService;
-  private AccountService accountService;
+
 
     @Autowired
-    public MainController(CookieService cookieService, UserService userService, WebSecurityConfig webSecurityConfig,
-                          UserDetailsServiceImpl userDetailsService, LoginService loginService,
-                          CheckoutService checkoutService,RegistrationService registrationService, AccountService accountService) {
+    public MainController(CookieService cookieService, UserService userService, LoginService loginService) {
 
         this.userService = userService;
         this.cookieService = cookieService;
-        this.webSecurityConfig = webSecurityConfig;
-        this.userDetailsService = userDetailsService;
         this.loginService = loginService;
-        this.checkoutService = checkoutService;
-        this.registrationService = registrationService;
-        this.accountService = accountService;
     }
 
 
@@ -69,39 +48,5 @@ public class MainController {
     }
 
 
-
-
-    @GetMapping("/account")
-    public String getUserByEmail(HttpServletResponse response, @RequestParam String email, Model model, @ModelAttribute User user) {
-         return accountService.getAccount(userService, model, email, cookieService, user, response);
-    }
-
-    @GetMapping("/store")
-    public String getStore() {
-        return "store";
-    }
-
-    @GetMapping("/register")
-    @ResponseStatus(HttpStatus.CREATED)
-    public String getRegister() {
-        return "register";
-    }
-
-
-    @PostMapping("/register")
-    @ResponseStatus(HttpStatus.CREATED)
-    public String postRegister(@ModelAttribute User user, Model model) throws EmailException {
-        return registrationService.registration(webSecurityConfig,userService,user,model);
-    }
-
-
-
-
-
-    @GetMapping("/checkout")
-    public String checkout(@ModelAttribute User user) {
-
-        return checkoutService.checkout(user);
-    }
 }
 
