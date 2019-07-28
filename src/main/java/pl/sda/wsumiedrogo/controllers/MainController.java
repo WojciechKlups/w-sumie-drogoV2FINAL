055ab19d7012eea4419a3c889f61ee3e315d9a01
Wojciech Klupś <wojciech.klups@gmail.com>
2019-07-28 15:30:20 +0200

    refactoring controllers

--- src/main/java/pl/sda/wsumiedrogo/controllers/MainController.java
+++ src/main/java/pl/sda/wsumiedrogo/controllers/MainController.java
@@ -1,26 +1,15 @@
 package pl.sda.wsumiedrogo.controllers;
 
-import org.apache.commons.mail.EmailException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
-import pl.sda.wsumiedrogo.model.Cart;
-import pl.sda.wsumiedrogo.model.User;
-import pl.sda.wsumiedrogo.security.UserDetailsServiceImpl;
-import pl.sda.wsumiedrogo.security.WebSecurityConfig;
-import pl.sda.wsumiedrogo.service.*;
-import org.springframework.web.bind.annotation.RequestParam;
-import pl.sda.wsumiedrogo.model.dto.UserDto;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.GetMapping;
+import pl.sda.wsumiedrogo.service.CookieService;
+import pl.sda.wsumiedrogo.service.LoginService;
+import pl.sda.wsumiedrogo.service.UserService;
 
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.security.Principal;
 
 @Controller
 public class MainController {
