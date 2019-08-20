e58fe06f64afafbd067c14864bd4fa8ddcdb2d5d
MateuszHaberla <Nateusz.Haberla22@gmail.com>
2019-08-20 18:40:22 +0200

    product controller

---
+++ src/main/java/pl/sda/wsumiedrogo/controllers/ProductController.java
@@ -0,0 +1,26 @@
+package pl.sda.wsumiedrogo.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import pl.sda.wsumiedrogo.service.ProductService;
+
+@Controller
+@RequestMapping("/products")
+public class ProductController {
+
+    private ProductService productService;
+
+    @Autowired
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping
+    public String allProducts(Model model){
+        model.addAttribute("products",productService.getAllProduct())
+    }
+
+}
