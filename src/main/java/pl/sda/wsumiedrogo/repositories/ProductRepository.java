ca9490d7cf6c803fb6ac008e947f8ea08b632d5a
MateuszHaberla <Nateusz.Haberla22@gmail.com>
2019-08-22 17:40:03 +0200

    Pagination is working. Trying to implements dynamic chaanginn of products in paginating pages.

--- src/main/java/pl/sda/wsumiedrogo/repositories/ProductRepository.java
+++ src/main/java/pl/sda/wsumiedrogo/repositories/ProductRepository.java
@@ -1,12 +1,12 @@
 package pl.sda.wsumiedrogo.repositories;
 
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import pl.sda.wsumiedrogo.model.Product;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product,Long> {
-    Page<Product> findByName(String name,PageRequest pageRequest);
+    Page<Product> findAll(Pageable pageable);
 }
--- src/main/java/pl/sda/wsumiedrogo/service/ProductService.java
+++ src/main/java/pl/sda/wsumiedrogo/service/ProductService.java
@@ -4,11 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestParam;
 import pl.sda.wsumiedrogo.model.Product;
 import pl.sda.wsumiedrogo.repositories.ProductRepository;
-
-import java.util.Optional;
 
 @Service
 public class ProductService {
@@ -23,7 +20,7 @@ public class ProductService {
     public void addNewProduct(Product product) {
         productRepository.save(product);
     }
-    public Page<Product> getAllProducts(String name){
-        return productRepository.findByName(name, PageRequest.of(0,5));
+    public Page<Product> getAllProducts(){
+        return productRepository.findAll(PageRequest.of(0,2));
     }
 }
